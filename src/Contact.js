a473be6f1069d00a5fba9141df9447885795c7d9
biryang <hwang7531@gmail.com>
2020-11-23 20:04:44 +0900

    add create data(name/phone)

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ContactDetails from './ContactDetails';
 import ContactInfo from './ContactInfo'
+import ContactCreate from './ContactCreate';
+
+import update from 'react-addons-update';
 
 class Contact extends Component {
 
@@ -26,6 +29,9 @@ class Contact extends Component {
 
     this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+    this.handleEdit = this.handleEdit.bind(this)
   }
 
   handleChange(e) {
@@ -39,6 +45,33 @@ class Contact extends Component {
       selectedKey: key
     })
     console.log(key, 'is selected')
+  }
+
+  handleCreate(contact) {
+    this.setState({
+      contactData: update(this.state.contactData,
+        { $push: [contact] })
+    })
+  }
+
+  handleRemove() {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        $splice: [[this.state.selectedKey, 1]]
+      }),
+      selectedKey: -1
+    })
+  }
+
+  handleEdit(name, phone) {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        [this.state.selectedKey]: {
+          name: { $set: name },
+          phone: { $set: phone }
+        }
+      })
+    })
   }
 
   render() {
@@ -72,6 +105,9 @@ class Contact extends Component {
         <ContactDetails
           isSelected={this.state.selectedKey != -1}
           contact={this.state.contactData[this.state.selectedKey]}
+        />
+        <ContactCreate
+          onCreate={this.handleCreate}
         />
       </div>
     );
---
+++ src/ContactCreate.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ContactCreate extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      phone: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange(e) {
+    let nextState = {}
+    nextState[e.target.name] = e.target.value
+    this.setState(nextState)
+  }
+
+  handleClick() {
+    const contact = {
+      name: this.state.name,
+      phone: this.state.phone
+    }
+    console.log(this.props.test, contact.name, contact.phone);
+
+    this.props.onCreate(contact)
+
+    this.setState({
+      name: '',
+      phone: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Create Contact</h2>
+        <p>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="phone"
+            placeholder="phone"
+            value={this.state.phone}
+            onChange={this.handleChange}
+          />
+        </p>
+        <button onClick={this.handleClick}>Create</button>
+      </div>
+    );
+  }
+}
+
+ContactCreate.propTypes = {
+  onCreate: PropTypes.func
+}
+
+ContactCreate.defaultProps = {
+  onCreate: () => {
+    console.error('onCreate not defined')
+  }
+}
+
+export default ContactCreate;
